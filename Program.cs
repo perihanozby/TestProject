bdded20c52b72cede7fb9c6f65f2b4f15f211774
perihanozby <fenerum7365@hotmail.com>
2024-01-06 00:27:06 +0300

    Rol yapma oyunu mücadelesi sınaması

--- Program.cs
+++ Program.cs
@@ -628,3 +628,35 @@ do
 Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");*/
 
 
+Console.WriteLine("______________________");
+
+string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter a string containing at least three characters:");
+
+do
+{
+    readResult = Console.ReadLine();
+    if(readResult != null)
+    {
+        if(readResult.Length >= 3)
+        {
+            validEntry = true;
+
+        }
+        else
+        {
+            Console.WriteLine("your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
+
+
+Console.WriteLine("______________________");
+
+int numericvalue = 0;
+bool validNumber = false;
+
+validNumber = int.TryParse(readResult, out numericvalue);
+
+
