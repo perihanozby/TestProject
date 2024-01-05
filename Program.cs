28f055aa0e5f1e4ba8f885df4f9bbff4d974e9e4
perihanozby <fenerum7365@hotmail.com>
2024-01-05 15:48:33 +0300

    Switch-case

--- Program.cs
+++ Program.cs
@@ -292,7 +292,7 @@ Console.WriteLine((random.Next(0, 2) == 0) ? "yazi" : "tura");*/
 
 ////  Alıştırma - Boole ifadelerini kullanarak sınama etkinliğini tamamlama
 
-string permission = "Admin|Manager";
+/*string permission = "Admin|Manager";
 int level = 55;
 
 
@@ -321,4 +321,161 @@ else if (permission.Contains("Manager"))
 else
 {
     Console.WriteLine("You do not have sufficient privileges.");
+}*/
+
+
+////7 Kod bloğunun içinde değişken oluşturma
+
+/*bool flag =true;
+int value = 0;
+if (flag)
+{
+    value= 10;
+    Console.WriteLine("Inside of code block:"+value);
 }
+Console.WriteLine("outside of code block:"+value);*/
+
+
+/*int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
+{
+    int total;
+
+    total += number;
+
+    if (number == 42)
+    {
+       bool found = true;
+
+    }
+
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");*/ // YERİNE
+
+/*int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");*/
+
+//////////////////////////////////////
+
+//                   SWİTCH_CASE              
+
+int employeelevel = 100;
+string employeename = "perihan ozbay";
+
+string title = "";
+
+switch (employeelevel)
+{
+    case 100:
+    //title = "Junior Associate";
+    //break;
+
+    case 200:
+    title = "Senior Associate";
+    break;
+
+    case 300:
+    title = "Manager";
+    break;
+
+    case 400:
+    title = "Senior manager";
+    break;
+    
+    default:
+    title = "Associate";
+    break;
+}
+
+Console.WriteLine($"{employeename},{title}");
+
+
+///////////////////////////////////////////
+
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+switch (product[0])
+{
+    case "01":
+    type = "Sweat shirt";
+    break;
+
+    case "02":
+    type = "Sweat shirt";
+    break;
+
+    case "03":
+    type = "Sweat shirt";
+    break;
+
+    default:
+    type = "Other";
+    break;
+    
+}
+
+
+switch (product[1])
+{
+    case "BL":
+    color = "Black";
+    break;
+
+    case "MN":
+    color = "Maroon";
+    break;
+    
+    default:
+    color = "White";
+    break;
+}
+
+switch(product[2])
+{
+    case "S":
+    size = "Small";
+    break;
+
+    case "M":
+    size = "Medium";
+    break;
+
+    case "L":
+    size ="Large";
+    break;
+
+    default:
+    size = "One Size Fits All";
+    break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
